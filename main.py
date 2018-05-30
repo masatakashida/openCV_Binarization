97e21b00d9a2836fde3ac8507d3106a82b386479
masataka <ar.tec.masa@gmail.com>
2018-05-30 23:11:25 +0900

    fix

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ cv2.namedWindow("img")
 threshold = 100
 cv2.createTrackbar("track", "img", threshold, 255, onTrackbar)
 while True:
-	ret, img_th = cv2.threshold(img, threshold, 255, cv2.THRESH_BINARY)
+	#ret, img_th = cv2.threshold(img, threshold, 255, cv2.THRESH_BINARY)
+	# それぞれの範囲で閾値作っていく
+	img_th = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 3, threshold)
 	cv2.imshow("img", img_th)
 	cv2.imshow("src", img)
 	# Escキーで抜ける
